3c0d4e1684d178816d131208ba824437dd9743a9
Duc Cuong <cuongtu532@gmail.com>
2023-02-09 10:37:37 +0700

    SuperAdmin Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './LoginPage';
-import RegisterPage from './RegisterPage';
-import CheckStatus from './CheckStatus';
+import LoginPage from './login/LoginPage';
+import RegisterPage from './apply/RegisterPage';
+import CheckStatus from './checkStatus/CheckStatus';
 import SuperAdminPage from './SuperAdminPage';
 
 import "./App.css";
